fe1c1e66de2a24957c86522cd93a739d67213f89
ruka-sarashina <novops911@gmail.com>
2023-06-26 14:21:39 +0700

    darkMode

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,61 @@
 // Navbar Fixed
 window.onscroll = () => {
-  const header = document.querySelector("header");
+  const header = document.querySelector('header');
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
 
   if (window.pageYOffset > fixedNav) {
-    header.classList.add("navbar-fixed");
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
   } else {
-    header.classList.remove("navbar-fixed");
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
   }
 };
 
 // Hamburger & Navbar
-const hamburger = document.querySelector("#hamburger");
+const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger-active");
-  navMenu.classList.toggle('hidden')
+// jika di klick maka akan memunculkan hamburger mobile
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
 });
+
+// Klick di luar hamburger
+// jika tidak di klick dalam nvabar/hamburgermenu, maka akan menutup
+window.addEventListener('click', (e) => {
+  if (e.target !== navMenu && e.target !== hamburger) {
+    hamburger.classList.remove('hamburger-active');
+    navMenu.classList.add('hidden');
+  }
+});
+
+// darkMode
+const darkMode = document.querySelector('#darkToggle');
+const html = document.querySelector('html');
+
+darkMode.addEventListener('click', () => {
+  if (darkMode.checked) {
+    html.classList.add('dark');
+    localStorage.theme = 'dark';
+  } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'white';
+  }
+});
+
+// pindah posisi toggle sesuai mode
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  darkMode.checked = true;
+} else {
+  darkMode.checked = false;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
       padding: '16px',
     },
     extend: {
-      colors : {
+      colors: {
         primary: '#2563eb',
-        second:'#64748b',
-        dark: '#0f172a'
+        second: '#64748b',
+        dark: '#0f172a',
       },
       screens: {
-        '2xl': '1320px'
-      }
+        '2xl': '1320px',
+      },
     },
   },
   plugins: [],
-}
+};
